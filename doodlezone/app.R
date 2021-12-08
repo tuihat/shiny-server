257aa19d36e9f2e4eecb20d0d7ca1e93ca98ccad
Laurel Childress <39061259+tuihat@users.noreply.github.com>
2021-12-08 10:58:30 -0600

    Add files via upload

--- doodlezone/app.R
+++ doodlezone/app.R
@@ -20,19 +20,110 @@ ui <- dashboardPage(
         tags$i("These are not official IODP-JRSO applications and functionality is 
          not guaranteed. User assumes all risk."), #italic disclaimer
         hr(style = "border-top: 1px solid #000000;"), #horizontal line
+        ########################################################################
         h4("General Applications", style = 'font-weight: bold;'), #subset of applications
+        fluidRow(
         userBox(
             title = userDescription(
-                title = "Elizabeth Pierce",
-                subtitle = "Web Designer",
+                title = "DSDP, ODP, IODP Coring Type Statistics",
+                subtitle = "",
                 image = "ops_app_image.jpg"
             ),
             status = "teal",
             closable = FALSE,
             maximizable = TRUE,
-            "Some text here!",
-            footer = "The footer here!"
+            tags$a(href="http://shinylaurel.com/shiny/Ops_DrillTypes/", 
+                   "Visit the application page.",
+                   target="_blank"), #another app link,
+            footer = "This application provides a graphic summary of drilling types including 
+            APC (advanced piston corer), HLAPC (half-length advanced piston corer), 
+            RCB (rotary core barrel), and XCB (extended core barrel). Recovery counts 
+            are available by program and expedition. Recovery with depth is also 
+            available by program and expedition. Scaled and unscaled graphs are available."
+        )),
+        ########################################################################
+        hr(style = "border-top: 1px solid #000000;"), #horizontal line
+        h4("At Sea Applications", style = 'font-weight: bold;'),
+        fluidRow(
+        userBox(
+            title = userDescription(
+                title = "EPM Daily Report",
+                subtitle = "",
+                image = "JR_icon.jpg"
+            ),
+            status = "maroon",
+            closable = FALSE,
+            maximizable = TRUE,
+            tags$a(href="http://shinylaurel.com/shiny/daily_report_maker/", 
+                   "Visit the application page.",
+                   target="_blank"), #another app link,
+            footer = "This application assists with the summary of coring information
+              for the EPM daily report. While at sea it will be better to run
+              this application locally, however you may preview it here."
         ),
+        userBox(
+            title = userDescription(
+                title = "Hydrocarbon Safety Monitoring",
+                subtitle = "",
+                image = "JR_icon.jpg"
+            ),
+            status = "maroon",
+            closable = FALSE,
+            maximizable = TRUE,
+            tags$a(href="http://shinylaurel.com/shiny/JRhydrocarbon/", 
+                   "Visit the application page.",
+                   target="_blank"), #another app link,
+            footer = "This application provides two sub-applications to assist with
+              hydrocarbon safety monitoring. One assists in the construction of
+              a thermal gradient. The other provides a graphic summary of 
+              hydrocarbon and temperature data, with guides for anomalous and
+              normal measurements. This application is not a replacement for 
+              geochemical knowledge or approval from appropriate drilling panels.
+              This application will also work better at sea if run locally."
+        )),
+        ########################################################################
+        hr(style = "border-top: 1px solid #000000;"), #horizontal line
+        h4("Post-Expedition Applications", style = 'font-weight: bold;'), #subset of applications
+        fluidRow(
+            userBox(
+                title = userDescription(
+                    title = "The 'cm-by-cm' Tool",
+                    subtitle = "",
+                    image = "coming_soon.jpg"
+                ),
+                status = "navy",
+                closable = FALSE,
+                maximizable = TRUE,
+                tags$a(href="https://web.iodp.tamu.edu/LORE/", 
+                       "Visit the application page.",
+                       target="_blank"), #another app link,
+                footer = "Convert section summaries to tables with rows
+           cm-by-cm. Add in splice and alternate depth scale information as desired."
+            )),
+        ########################################################################
+        hr(style = "border-top: 1px solid #000000;"), #horizontal line
+        h4("GCR Applications", style = 'font-weight: bold;'), #subset of applications
+        fluidRow(
+            userBox(
+                title = userDescription(
+                    title = "GCR Sample Planning",
+                    subtitle = "",
+                    image = "GCR_app_image.jpg"
+                ),
+                status = "purple",
+                closable = FALSE,
+                maximizable = TRUE,
+                tags$a(href="https://shinylaurel.com/shiny/GCR_sample_planning/", 
+                       "Visit the application page.",
+                       target="_blank"), #another app link,
+                footer = "This application provides several tools for sample planning
+              purposes. (1) The user provides, singularly or in batch, sample
+              requests by 'mbsf' scale (m CSF-A) and the application returns a
+              samples list with appropriate sample IDs. (2) The user supplies a
+              range of cores and sample interval and the application returns a 
+              sample list of repetitive sampling. (3) The user provides a density
+              and either a volume or mass and the alternative is returned."
+            )),
         tags$head(tags$style(HTML('
         /* logo */
         .skin-blue .main-header .logo {
