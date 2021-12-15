608a0ffb859fcac93be1cadb7a8df006ce4b96fa
Laurel Childress <39061259+tuihat@users.noreply.github.com>
2021-12-15 17:27:51 -0600

    Add files via upload

--- doodlezone/app.R
+++ doodlezone/app.R
@@ -1,92 +1,36 @@
-#DSDP, ODP, IODP Expedition Map
-#started: 9 December 2021
-#updated: 10 December 2021
-#Laurel Childress; childress@iodp.tamu.edu
-
-###############################################################################
-# A very basic map making tool for ocean drilling sites.
-###############################################################################
-
-#Packages
-if(!require(rmarkdown)){
-  install.packages("rmarkdown")
-  library(rmarkdown) #rmarkdown
+library(dplyr)
+if(!require(DT)){ #check if the package is installed and sourced
+  install.packages("DT") #if not, install the package
+  library(DT) #and source the package 
 }
+library(shiny)
+library(rhandsontable)
+library(shinydashboard)
+library(shinydashboardPlus)
 
-if(!require(shiny)){ #check if the package is installed and sourced
-  install.packages("shiny") #if not, install the package
-  library(shiny) #and source the package
-}
-
-if(!require(shinyjs)){ #check if the package is installed and sourced
-  install.packages("shinyjs") #if not, install the package
-  library(shinyjs) #and source the package
-}
-
-if(!require(shinyWidgets)){ #check if the package is installed and sourced
-  install.packages("shinyWidgets") #if not, install the package
-  library(shinyWidgets) #and source the package
-}
-
-if(!require(shinydashboard)){ #check if the package is installed and sourced
-  install.packages("shinydashboard") #if not, install the package
-  library(shinydashboard) #and source the package
-}
-
-if(!require(shinydashboardPlus)){ #check if the package is installed and sourced
-  install.packages("shinydashboardPlus") #if not, install the package
-  library(shinydashboardPlus) #and source the package
-}
-
+table_template <- read.csv("table_template.csv", colClasses = c("character", "integer", "factor"))
 volumes <- read.csv("sample_volume.csv")
 
 ui <- dashboardPage(
-  title = "Sample Volume Calculations", #website title
   header = dashboardHeader(title = "Easy Sample Volume Calculations"),
   sidebar = dashboardSidebar(width = "0px"),
   body = dashboardBody(
     fluidRow(
-      box(width = 5,
-          fluidRow(
-            column(width = 6, 
-                   numericInput("slen1", label = "Enter sample length(s) in cm:", value = 1,
-                                min = 1, max = 150, step = 1), br(),
-                   numericInput("slen2", label = NULL, value = 2,
-                                min = 1, max = 150, step = 1), br(),
-                   numericInput("slen3", label = NULL, value = 3,
-                                min = 1, max = 150, step = 1),
-                   "Use integers only, please."),
-            column(width = 6,
-                   selectInput("tool1", "Choose your tool:",
-                               c("WRND", "SHLF", "QRND"),
-                               multiple = FALSE), br(),
-                   selectInput("tool2", label = NULL,
-                               c("WRND", "SHLF", "QRND"),
-                               multiple = FALSE), br(),
-                   selectInput("tool3", label = NULL,
-                               c("WRND", "SHLF", "QRND"),
-                               multiple = FALSE))
-          )),
-      box(width = 2, 
-          img(src="U4T.gif")),
-      box(width = 5,
-          fluidRow(
-            column(width = 6, align="center",
-                   tags$b("Sediment (APC, XCB) volume (cc)"),
-                   textOutput('sed1'), br(), br(), br(),
-                   textOutput('sed2'), br(), br(), br(),
-                   textOutput('sed3')
-                    ),
-            column(width = 6, align="center",
-                   tags$b("Rock (RCB) volume (cc)"),
-                   textOutput('rcb1'),br(), br(), br(),
-                   textOutput('rcb2'), br(), br(), br(),
-                   textOutput('rcb3'))
-          ))
+      column(width = 4,
+             h2("Paste your data in here."),
+             h3("Examples are given in the first three columns. Non-integer values provided
+       for sample length will be truncated to integers."),
+             wellPanel(
+      rHandsontableOutput("table"))
     ),
-    "--------------------------------------------------------------------------", br(),
-    "Will add some sort of thing that is either a batch processor or if I can figure it
-    out a table that you can paste into (fancy fancy).", br(),
+    column(width = 1),
+    column(width = 6,
+           h2("Results are over here."),
+           h3("SED applies to APC and XCB material (F, H, X). RCB applies to RCB
+              material (R)."), br(),
+           DT::dataTableOutput("resultstable"),
+           downloadButton("download1.1", "Download results")),
+    column(width = 1)),
     br(),
     tags$i("These are not official IODP-JRSO applications and functionality is 
            not guaranteed. User assumes all risk."), #italic disclaimer
@@ -116,46 +60,57 @@ ui <- dashboardPage(
 )
 
 server <- function(input, output, session) {
-#########---Results FIRST row---####################################  
-  the_first <- reactive({
-    df <- subset(volumes, length_cm == as.integer(input$slen1))
-    df2 <- df[grepl(input$tool1, names(df))]
-    df2
+  
+  df <- table_template
+  
+  datavalues<-reactiveValues(data=df)
+  
+  output$table <- renderRHandsontable({
+    
+    mytab <- rhandsontable(datavalues$data, 
+                           colHeaders = c("Sample ID", "Sample Length (cm)", "Tool"), 
+                           stretchH = "all")
+    return(mytab)
   })
   
-  output$sed1 <- renderText({ 
-    df <- the_first()
-    as.character(df[1,1]) })
-  output$rcb1 <- renderText({ 
-    df <- the_first()
-  as.character(df[1,2]) })
+  do_data_stuff <- reactive({
+    tmp_df <- hot_to_r(input$table)
+    tmp_list <- list()
+    for(i in 1:nrow(tmp_df)){
+      df_row <- tmp_df[i,]
+      tmp_vols <- subset(volumes, length_cm == as.integer(df_row$Sample.length..cm.))
+      tmp_vols2 <- tmp_vols[grepl(df_row$Tool, names(tmp_vols))]
+      df_row$SED_cc <- as.character(tmp_vols2[1,1])
+      df_row$RCB_cc <- as.character(tmp_vols2[1,2])
+      tmp_list[[i]] <- df_row
+    }
+    new_df <- do.call("rbind", tmp_list)
+    new_df
+  })
+  
+  output$resultstable <- DT::renderDataTable({
+    pretty_table <- do_data_stuff()
+    names(pretty_table)[1] <- "Sample ID"
+    names(pretty_table)[2] <- "Sample Length (cm)"
+    names(pretty_table)[3] <- "Tool"
+    names(pretty_table)[4] <- "SED volume (cc)"
+    names(pretty_table)[5] <- "RCB volume (cc)"
+    DT::datatable(pretty_table, options = list(pageLength = 20, scrollX = TRUE,
+                                               columnDefs = list(list(className = 'dt-center', targets = "_all"))), 
+                                                rownames= FALSE) %>% 
+      formatRound(columns = c(4:5), digits = 1)
+  })
+  
+  #################################################
+  output$download1.1 <- downloadHandler( # Downloadable csv (single file) ----
+                         filename = function() {
+                           paste("volume_results", ".csv", sep = "")
+                         },
+                         content = function(file) {
+                           write.csv(do_data_stuff(), file, row.names = FALSE)
+                         })
 
-#########---Results SECOND row---####################################  
-  the_second <- reactive({
-    df <- subset(volumes, length_cm == as.integer(input$slen2))
-    df2 <- df[grepl(input$tool2, names(df))]
-    df2
-  })
-  
-  output$sed2 <- renderText({ 
-    df <- the_second()
-    as.character(df[1,1]) })
-  output$rcb2 <- renderText({ 
-    df <- the_second()
-    as.character(df[1,2]) })
-#########---Results SECOND row---####################################  
-  the_third <- reactive({
-    df <- subset(volumes, length_cm == as.integer(input$slen3))
-    df2 <- df[grepl(input$tool3, names(df))]
-    df2
-  })
-  
-  output$sed3 <- renderText({ 
-    df <- the_third()
-    as.character(df[1,1]) })
-  output$rcb3 <- renderText({ 
-    df <- the_third()
-    as.character(df[1,2]) })
+   
 }
 
 shinyApp(ui = ui, server = server)
