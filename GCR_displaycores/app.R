074522a9b92fa60661b0d414c295d99a517f51db
Laurel Childress <39061259+tuihat@users.noreply.github.com>
2024-01-16 14:18:28 -0600

    Add files via upload

--- GCR_displaycores/app.R
+++ GCR_displaycores/app.R
@@ -577,7 +577,9 @@ ui <- dashboardPage(
     tags$i("These are not official IODP-JRSO applications 
                                     and functionality is not guaranteed. User assumes all risk."), #italic disclaimer
   br(),
-  tags$i("Questions, comments, concerns, compliments: shinylaurelwebmaster@gmail.com")
+  tags$i("Questions, comments, concerns, compliments: shinylaurelwebmaster@gmail.com"),
+  br(),
+  tags$i("This app can be cited by https://doi.org/10.5281/zenodo.10498831")
   )
 )
 
